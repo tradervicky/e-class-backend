c0b2e1f4dab6fb06085e4ac59f3a424933a0ce9e
Vicky Kumar Gupta <vickygupta031@gmail.com>
2024-07-29 09:12:58 +0530

    user signin api created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const users = require('../models/user')
+const secret = process.env.JWT_SECRET
 exports.userSignup = async (req, resp)=>{
     try {
     const {name, email, password} = req.body;
@@ -23,4 +25,25 @@ catch(error){
     console.log(error)
     resp.status(500).json({msg : "Server error"})
 }
+}
+
+exports.userLogin = async (req, res)=>{
+    try {
+        const {email , password} = req.body;
+        const findUser = await users.findOne({email})
+        if(!findUser){
+            res.status(400).json({msg : "Please enter correct email"})
+        }
+        const matchPassword = bcrypt.compare(password, findUser.password)
+        if(!matchPassword){
+            res.status(400).json({msg : "Please enter correct Password"})
+        }
+        const payload = {
+            id : findUser._id,
+            name : findUser.name, email, purchasedCourses : findUser.purchasedCourses}
+        const token = jwt.sign(payload, secret, {expiresIn: '1h'})
+        res.status(200).json({msg: "User loggded in successfully", data : payload, token})
+    } catch (error) {
+        return res.status(401).json({msg : "Server error"})
+    }
 }
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {userSignup} = require('../controllers/userController')
+const {userSignup, userLogin} = require('../controllers/userController')
 
 
 router.post('/signup', userSignup)
+router.post('/signin', userLogin)
 
 module.exports = router;
